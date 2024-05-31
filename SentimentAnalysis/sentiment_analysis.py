e3d7297d0c4226e18b35a5dc8aeaddcdff1c7621
VarshaK <varsha.kamble61@gmail.com>
2024-05-31 18:58:46 +0100

    Updated erroe handling

--- SentimentAnalysis/sentiment_analysis.py
+++ SentimentAnalysis/sentiment_analysis.py
@@ -2,19 +2,23 @@ import requests
 import json
 #https://developer.ibm.com/apis/catalog/embeddableai--watson-natural-language-processing-apis/api/API--embeddableai--watson-natural-language-processing-apis#SentimentPredict
 def sentiment_analyzer(text_to_analyse):
+        """"creating the sentiment analysis application"""
         #url = "https://sn-watson-sentiment-bert.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
         url = #https://localhost:8080/v1/watson.runtime.nlp.v1/NlpService/v1/watson.runtime.nlp.v1/NlpService/SentimentPredict"
         header = {"grpc-metadata-mm-model-id": "sentiment_aggregated-bert-workflow_lang_multi_stock"}
-        myobj = { "raw_document": { "text": text_to_analyse } }
+        myobj = {"raw_document": { "text": text_to_analyse } }
+        
         response = requests.post(url, json = myobj, headers = header)
        
         # format response
         formatted_response = json.loads(response.text)
-       
+        if response.status_code == 200:
         #get value of dictionary keys label and score
-        label = formatted_response['documentSentiment']['label']
-        score = formatted_response['documentSentiment']['score']
-        
+            label = formatted_response['documentSentiment']['label']
+            score = formatted_response['documentSentiment']['score']
+        else:
+            label = None
+            score = None
         #return response
         #return formatted_response
         # return dictionary
